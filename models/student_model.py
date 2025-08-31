395a69766e180777cbfbe0fb3304127cb2960d5f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-09-01 01:44:51 +0530

    feat: Implement student schema model

---
+++ models/student_model.py
@@ -0,0 +1,25 @@
+from mongoengine import Document, StringField, DateTimeField, ListField, DictField, IntField
+from datetime import datetime
+
+class Student(Document):
+    phone = StringField(required=True)
+    email = StringField(required=True)
+    name = StringField(required=True)
+    roll_number = StringField(required=True) 
+    section = StringField(required=True)
+    semester = StringField(required=True)
+    batch = StringField(required=True)
+    course = StringField(required=True)
+    branch = StringField(required=True)
+    face_id = StringField(required=True)
+    # List of dictionaries, each with 'subject_code' and 'attendance_percentage' (integer)
+    sub_attendance = ListField(
+        DictField(
+            fields={
+                'subject_code': StringField(required=True),
+                'attendance_percentage': IntField(min_value=0, max_value=100, required=True)
+            }
+        ),
+        default=list
+    )
+    created_at = DateTimeField(default=datetime.utcnow)
